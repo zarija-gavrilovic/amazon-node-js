ada5635977355533f634323f175ea6047766ef3b
zarija-gavrilovic <58690622+zarija-gavrilovic@users.noreply.github.com>
2020-12-11 11:58:09 +0100

    First commit

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+
+const PORT = 3000;
+app.set('view engine','ejs');
+
+
+app.get('/',(req,res) => {
+    res.render('index')
+})
+
+app.listen(PORT, () => {
+    console.log(`Server radi na portu ${PORT}`);
+})
+
